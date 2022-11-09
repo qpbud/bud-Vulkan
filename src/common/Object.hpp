8d4370510e793e4c0a7edec6c180eaf46db024c3
qpbud <qpbud@outlook.com>
2022-11-10 00:34:04 +0800

    [common]add some common objects
    Signed-off-by: qpbud <qpbud@outlook.com>

--- src/common/Object.hpp
+++ src/common/Object.hpp
@@ -1,5 +1,24 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
+#include <vulkan/vk_icd.h>
+namespace bud::vk {
 
-struct VkInstance_T {};
+struct DispatchableObject {
+    VK_LOADER_DATA loaderData;
+
+    DispatchableObject() : loaderData() {
+        set_loader_magic_value(&loaderData);
+    }
+};
+
+}
+
+struct VkInstance_T : public bud::vk::DispatchableObject {};
+
+namespace bud::vk {
+
+template<typename T>
+class Object : public T {};
+
+}
